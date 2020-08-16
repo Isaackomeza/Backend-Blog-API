5a4037d32857a49d2bd2c74e1ff4e79831f36a41
Isaac <isaackomeza@gmail.com>
2020-08-16 17:10:01 +0200

    Implementing get and post api

---
+++ index.js
@@ -0,0 +1,49 @@
+const Joi = require('joi');
+const cors = require('cors');
+
+const express = require('express')
+const app = express();
+app.use(express.json());
+
+const blogs = [
+  {id: 1, name: 'blog1'},
+  {id: 2, name: 'blog2'},
+  {id: 3, name: 'blog3'},
+];
+app.use(cors())
+app.get('/', (req, res)=> {
+  res.send('Hello World!!!');
+})
+
+app.get('/api/blogs', (req, res)=> {
+  res.send(blogs);
+})
+
+app.get('/api/blogs/:id', (req, res) => {
+  const blog = blogs.find(b => b.id === parseInt(req.params.id));
+  if (!blog) {
+  res.status(404).send('The blog with the given ID was not found');
+  }else{
+    res.send(blog);
+  };
+});
+const port = process.env.PORT || 3000
+app.listen(3000, ()=> console.log(`Listening on port ${port}....`));
+
+app.post('/api/blogs', (req, res) => {
+  const name = { name: req.body.name };
+  const schema = Joi.object({ name: Joi.string().min(3).required() });
+
+  const result = schema.validate(name);
+
+  if (result.error){
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+  const blog = {
+    id: blogs.length + 1,
+    name: req.body.name
+  };
+  blogs.push(blog);
+  res.send(blog);
+});
