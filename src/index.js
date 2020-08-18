03bb40cad212b06e99dc4c33ae6c89e7148d4e79
Isaac <isaackomeza@gmail.com>
2020-08-18 17:23:12 +0200

    Adding Error handling status and message

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import {createError} from 'http-errors';
 import Mroute from './routes/messages-routes';
 import Blogroute from './routes/blogs-routes';
 import Userroute from './routes/users-routes';
@@ -10,11 +11,27 @@ const port = process.env.PORT || 3000;
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
-server.get('/', (req, res) => res.status(200).json({ message: 'Welcome to my brand' }));
+server.get('/', (req, res) => res.status(200).json({ 
+    message: 'Welcome to my brand' 
+}));
 server.use('/', Mroute);
 server.use('/', Blogroute);
 server.use('/', Userroute);
 server.use('/', Commentroute);
+
+// server.use((req, res, next) => {
+//     next(createError.NotFound('Not found'));
+// });
+// server.use(async (error, req, res, next) => {
+//     res.status = error.status || 404;
+//     res.send({status: error.status, message: error.message});
+// })
+server.use(async (req, res) => {
+    const error = await new Error('Not found');
+    error.status = 404;
+    res.send({status: error.status, message: error.message});
+});
+
 
 server.listen(port, console.log(`server listening on ${port}`));
 export default server;
