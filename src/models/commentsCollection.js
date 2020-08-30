1fdd932d96f76e8893e657c3421f62a8b9c8c8b0
Isaac <isaackomeza@gmail.com>
2020-08-30 11:44:35 +0200

    Adding validation to comment

--- src/models/commentsCollection.js
+++ src/models/commentsCollection.js
@@ -21,8 +21,8 @@ const mongoose = require('mongoose');
 
 const commentSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: String,
-    email: String,
-    comment: String
+    name: {type: String, required: true },
+    email: {type: String, required: true },
+    comment: {type: String, required: true }
 });
 module.exports = mongoose.model('Comment', commentSchema)
