0ead675d6b6ac42054999fd09a8c8e1860bbc14a
Isaac <isaackomeza@gmail.com>
2020-08-30 18:49:20 +0200

    removing unneccesary files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import {createError} from 'http-errors';
 import mongoose from 'mongoose';
 const userRoutes = require('./routes/user');
 require("dotenv").config();
@@ -10,19 +9,10 @@ import bodyParser from 'body-parser'
 
 import Mroute from './routes/messagesRoutes';
 import Blogroute from './routes/blogsRoutes';
-import Userroute from './routes/usersRoutes';
 import Commentroute from './routes/commentsRoutes';
-import Authroute from './routes/auth.routes';
 
-// mongoose.connect(`mongodb+srv://isaackomeza:${process.env.MONGO_ATLAS_PW}@cluster0.se57y.mongodb.net/Mybackend?retryWrites=true&w=majority`, 
-// {
-//     userNewUrlParser: true,
-//     userUnifiedTopology: true
-// });
 const db = 'mongodb+srv://Isaac:' + process.env.MONGO_ATLAS_PWD + '@cluster0.se57y.mongodb.net/Mydatabase?retryWrites=true&w=majority'
-console.log(db)
-// const db = 'mongodb+srv://Isaac:64WPo8tNTOVpfR9a@cluster0.se57y.mongodb.net/Mydatabase?retryWrites=true&w=majority'
-console.log(db)
+
 mongoose.connect( db,{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true} )
 .then(() => console.log('MongoDB connected...'))
 .catch(err => console.log(err));
@@ -43,7 +33,6 @@ server.use('/', Mroute);
 server.use('/', Blogroute);
 server.use('/', Userroute);
 server.use('/', Commentroute);
-// server.use('/', Authroute);
 server.use("/user", userRoutes);
 
 
